d9f6e8f71bb4d9e331a096a6213cc55d271172ba
Anton Sazhin <antonsazhin@users.noreply.github.com>
2020-12-06 22:06:38 -0800

    Adding example of importing functions from a different file

---
+++ function_import/main.py
@@ -0,0 +1,12 @@
+from somemath import *
+
+
+def main():
+    x = 2
+    y = 3
+    print("Sum : " + str(sum_of(x, y)))
+    print("Product : " + str(product_of(x, y)))
+
+
+if __name__ == '__main__':
+    main()
---
+++ function_import/somemath.py
@@ -0,0 +1,7 @@
+
+def sum_of(x, y):
+    return x + y
+
+
+def product_of(x, y):
+    return x * y
